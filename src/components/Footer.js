d43dd3ef43a28dcaffcb0979b3fe65c66a64e953
Jideofor <njusticej@gmail.com>
2020-04-22 18:03:42 +0100

    feat: add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="row">
+      <p className="text-muted credit">
+        All images are from{" "}
+        <a href="http://commons/wikimedia.org/wiki/Main">wikimedia commons</a>
+        and are in the public domain
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
