4cd50f46db67d467201bde7cc56608986039dada
Jideofor <njusticej@gmail.com>
2020-04-22 18:03:27 +0100

    feat: add continue component

---
+++ src/components/Continue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Continue = () => {
+return(
+    <div className = "row">
+    <div className="jumbotron col-10 offset-1">
+        <h1>Continue</h1>
+        <p>Continue</p>
+    </div>
+    </div>
+)
+}
+
+export default Continue
