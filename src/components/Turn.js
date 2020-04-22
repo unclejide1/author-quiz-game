e1171ea938ed5d4fc9ab32945a350ba59e12f834
Jideofor <njusticej@gmail.com>
2020-04-22 18:04:12 +0100

    feat: add turn component

---
+++ src/components/Turn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Turn = () => {
+return(
+    <div className = "row">
+    <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>select the book written by the author shown</p>
+    </div>
+    </div>
+)
+}
+
+export default Turn
