92f2caa685b9beb371725a466504c6c12140f796
Jideofor <njusticej@gmail.com>
2020-04-22 18:03:54 +0100

    feat: add hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Hero = () => {
+return(
+    <div className = "row">
+    <div className="jumbotron col-10 offset-1">
+        <h1>Author Quiz</h1>
+        <p>select the book written by the author shown</p>
+    </div>
+    </div>
+)
+}
+
+export default Hero
